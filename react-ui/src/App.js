7a8a79d19f18ed56b5abc7ff27f4c60b2319b78f
Oleh <oleh.fylypiv@gmail.com>
2017-10-19 20:06:19 +0300

    "Сonfigured React environment + interaction with Express"

---
+++ react-ui/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+ 
+    render() {
+        return (
+            <div>
+                <h2>Hello World!</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
